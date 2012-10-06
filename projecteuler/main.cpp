caa64de0ad9a82503afdf46e37c82dd6a36dc301
Bradley Bosher <Brad@Bradley-Boshers-MacBook-Air.local>
2012-10-07 00:21:52 +0100

    Initial Commit

---
+++ projecteuler/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  projecteuler
+//
+//  Created by Bradley Bosher on 07/10/2012.
+//  Copyright (c) 2012 Bradley Bosher. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
